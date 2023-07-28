298afa98fc474d6066d9cf72edf75065a9a9ca91
Kailas Puri <kailaspuri123@gmail.com>
2023-07-28 15:44:30 +0530

    chnage folder name

--- src/components/BasicTable.js
+++ src/components/BasicTable.js
@@ -1,49 +1,19 @@
-import React, { useMemo } from "react";
-import { useTable } from "react-table";
-import MOCK_DATA from "./MOCK_DATA.json";
-import { COLUMNS } from "./Column";
+import { useState } from "react";
 import "./TableStyle.css";
+import { useEffect } from "react";
+import axios from "axios";
 
-// export const BasicTable = () => {
-//   const columns = useMemo(() => COLUMNS, []);
-//   const data = useMemo(() => MOCK_DATA, []);
-
-//   const tableInstance = useTable({
-//     columns,
-//     data,
-//   });
-
-//   const { getTableProps, getTableBodyProps, headerGroup, rows, prepareRow } =
-//     tableInstance;
-//   return (
-//     <table {...getTableProps()}>
-//       <thead>
-//         {headerGroup.map((headerGroup) => (
-//           <tr {...headerGroup.getHeaderGroupProps()}>
-//             {headerGroup.headers.map((column) => (
-//               <th {...column.getHeaderProps()}>{column.render("Header")}</th>
-//             ))}
-//           </tr>
-//         ))}
-//       </thead>
-//       <tbody {...getTableBodyProps()}>
-//         {rows.map((row) => {
-//           prepareRow(row);
-//           return (
-//             <tr {...row.getRowProps()}>
-//               {row.cells.map((cell) => {
-//                 return <td {...cell.getCellProps()}>{cell.render("cell")}</td>;
-//               })}
-//             </tr>
-//           );
-//         })}
-//       </tbody>
-//     </table>
-//   );
-// };
+const URL = `http://localhost:3001/users`;
 
 function BasicTable() {
-  const users = useMemo(() => MOCK_DATA, []);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(URL)
+      .then((res) => setUsers(res.data))
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <div>
